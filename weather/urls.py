79fdbf6050d5b62e86c01d431ede4e9fbf4badd2
Milos Radevic <milos.radevic3@gmail.com>
2020-01-07 15:46:32 +0100

    Geolocation

--- weather/urls.py
+++ weather/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import homepage, search, detail
+from .views import homepage, search, detail, location
 
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("search/", search, name="search"),
-    path("search/<int:id>/", detail, name="detail")
+    path("search/<int:id>/", detail, name="detail"),
+    path("location/", location, name="location")
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from requests import get
 from json import load
 from django.http import JsonResponse
@@ -29,24 +29,46 @@ def homepage(request):
 
 
 def search(request):
-    # Point is to get city from pre-downloaded json file and then send that to the url to get temperature
-    global API_KEY
-    queryID, data = "", {"result": "empty set"}
-    q = request.GET.get("query")
-    if q:
-        data["result"] = search_query(query=q)
+    if request.is_ajax():
+        # Point is to get city from pre-downloaded json file and then send that to the url to get temperature
+        global API_KEY
+        queryID, data = "", {"result": "empty set"}
+        q = request.GET.get("query")
+        if q:
+            data["result"] = search_query(query=q)
+        else:
+            data["result"] = []
+        return JsonResponse(data=data)
     else:
-        data["result"] = []
-    return JsonResponse(data=data)
+        return redirect(to="homepage")
 
 
 def detail(request, id: int):
     data = {"result": "", "icon": ""}
-    response = get("http://api.openweathermap.org/data/2.5/weather?id=" + str(id) + "&APPID=" + API_KEY)
+    response = get(url="http://api.openweathermap.org/data/2.5/weather?id=" + str(id) + "&APPID=" + API_KEY)
     if response.status_code == 200:
+        log(response.text)
         data["result"] = eval(response.text)
         data["icon"] = "http://openweathermap.org/img/w/" + data["result"]["weather"][0]["icon"] + ".png"
         return render(request=request, template_name="weather/detail.html", context={"data": data})
     else:
         warning(request=request, message=eval(response.text)["message"])
         return render(request=request, template_name="weather/homepage.html")
+
+
+def location(request):
+    coo = {"x": None, "y": None, "msg": ""}
+    if request.is_ajax():
+        coo["x"] = request.GET.get("Latitude")
+        coo["y"] = request.GET.get("Longitude")
+        if coo["x"] and coo["y"]:
+            response = get(
+                url="http://api.openweathermap.org/data/2.5/weather?lat=" + 
+                str(coo["x"]) + "&lon=" + str(coo["y"]) + "&APPID=" + API_KEY
+            )
+            return JsonResponse(data={"location": eval(response.text)})
+        else:
+            coo["msg"] = "Coordinates not found."
+        return JsonResponse(data=coo)
+    else:
+        return redirect(to="homepage")
